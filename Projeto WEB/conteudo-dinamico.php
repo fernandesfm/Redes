70f87e26c6978e01503db3b5368b8c563f60e66b
Felipe Fernandes <feelipefernandes@live.com>
2023-10-25 01:55:13 -0300

    Update conteudo-dinamico.php

--- Projeto WEB/conteudo-dinamico.php
+++ Projeto WEB/conteudo-dinamico.php
@@ -1,1 +1,22 @@
-A
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Conteúdo Dinâmico</title>
+</head>
+<body>
+    <h1>Informações Dinâmicas</h1>
+    <p>IP: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+    <p>Data e Hora: <span id="data-hora"></span>
+
+    <script>
+        function atualizarDataHora() {
+            var dataHoraElement = document.getElementById("data-hora");
+            var dataHora = new Date();
+            dataHoraElement.textContent = dataHora.toLocaleString();
+        }
+
+        atualizarDataHora();
+        setInterval(atualizarDataHora, 1000); // Atualiza a cada segundo
+    </script>
+</body>
+</html>
